7025057fc48877d85e45428f6d5bd9251f64cf57
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 <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>
2023-06-14 09:05:07 +0530

    node cron added

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const WebSocket = require("ws");
 const cron = require("node-cron");
 
 function app() {
+  console.log("Cron job started");
   const client = new WebSocket("wss://fambuddy.onrender.com");
   client.on("pong", (data) => {
     console.log("NODE CRON - fambuddy");
